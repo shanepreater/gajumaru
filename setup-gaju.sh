c51e7717841b3bd78c1fd0292f8218e1c32075ab
Shane P <shane@argodevops.co.uk>
2025-05-24 18:38:42 +0100

    chore: made steps more verbose

--- setup-gaju.sh
+++ setup-gaju.sh
@@ -5,16 +5,17 @@
 FILE="$( realpath $0 )"
 INSTALL_DIR="$(dirname $FILE)"
 
-echo "File is $FILE and directory is $INSTALL_DIR"
-
-# 1. Install the ZX system
+echo "# 1. Install the ZX system"
 wget -q https://zxq9.com/projects/zomp/get_zx && bash get_zx
 
-# 2. Import the realms
+echo "# 2. Ensure the system is upgraded."
+~/bin/zx upgrade
+
+echo "# 3. Import the realms"
 zx import realm uwiger.zrf
 zx import realm qpq.zrf
 
-# 3. Setup the Desktop links
+echo "# 3. Setup the Desktop links"
 cat > ~/Desktop/GajuDesk.desktop <<EOF
 [Desktop Entry]
 Version=1.0
