6f26e6fd852905f0000c69bd01deb9f74237150a
Shane P <shane@argodevops.co.uk>
2025-06-08 16:48:53 +0100

    feat: moving to using the config file

--- setup-headless-miner.sh
+++ setup-headless-miner.sh
@@ -8,13 +8,46 @@
 FILE="$( realpath $0 )"
 INSTALL_DIR="$(dirname $FILE)"
 
-if [ $# -ne 1 ]; then
-  echo "Error running script, public key should be passed as the only argument!" 1>&2;
+show_usage() {
+  echo "$FILE <pubkey> <miners>" 1>&2;
+  echo "Where" 1>&2;
+  echo "  pubkey - the public key for the mining account to use" 1>&2;
+  echo "  miners - the number of concurrent workers to use" 1>&2;
+}
+
+write_config() {
+  path=$1
+  key=$2
+  miners=$3
+  echo " Writing config to $path with key: $key and miners: $miners"
+
+  echo "{" > $path
+  echo "  \"pubkey\": \"$key\"," >> $path
+  echo "  \"workers\": [" >> $path
+  for i in $(seq $miners); do
+    if [ $i -gt 1 ]; then
+      echo "    ,{\"executable\": \"mean29-generic\"}" >> $path
+    else
+      echo "    {\"executable\": \"mean29-generic\"}" >> $path
+    fi
+  done
+  echo "  ]" >> $path
+  echo "}" >> $path
+}
+
+if [ $# -ne 2 ]; then
+  show_usage
+  echo "Error running script, public key and number of concurrent workers should be passed in." 1>&2;
   exit 1
 fi
 
-echo "Setting the public key..."
+echo "Setting the run variables"
 pubkey=$1
+miners=$2
+config_dir=/usr/local/gajuminer
+config_file=$config_dir/gmhive_client_config.json
+service_dir=/etc/systemd/system/
+zomp_file=uwiger-gmhive_client
 
 echo "Checking the zomp/zx is installed..."
 found=$(which zx)
@@ -55,10 +88,21 @@ echo "Ensuring everything is up to date"
 ${script_path}/zx upgrade
 
 echo "Starting mining using public key: \$pubkey"
-${script_path}/zx run uwiger-gmhive_client -gmhc pubkey \$pubkey
+${script_path}/zx run $zomp_file -gmhc pubkey \$pubkey -setup data_dir $config_dir
 EOF
+
 chmod 755 $script_path/headless-miner.sh
 
+echo "Ensuring the gajuminer directory exists..."
+sudo mkdir -p $config_dir
+sudo chmod 755 $config_dir
+
+echo "Creating the configuration file..."
+write_config ~/config.json $pubkey $miners
+sudo cp ~/config.json $config_file
+sudo chmod 644 $config_file
+
+echo "Creating the daemon service..."
 cat > ~/gajuminer.service <<EOF
 [Unit]
 Description=Headless Gajumaru mining service
@@ -74,7 +118,7 @@ WantedBy=multi-user.target
 EOF
 
 echo "enabling the service for automatic execution..."
-sudo cp ~/gajuminer.service /etc/systemd/system/
+sudo cp ~/gajuminer.service $service_dir
 sudo systemctl daemon-reload
 sudo systemctl enable gajuminer
 
